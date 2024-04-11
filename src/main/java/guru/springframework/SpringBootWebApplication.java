330e55bff7a9a5a914d056f363a2cf0140efb595
nkanyisom <nkanyimalunga@gmail.com>
2024-04-11 12:45:07 +0200

    Update SpringBootWebApplication.java for standalone tomcat deployment

--- src/main/java/guru/springframework/SpringBootWebApplication.java
+++ src/main/java/guru/springframework/SpringBootWebApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootWebApplication {
+public class SpringBootWebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
